0568c3bbf90a3f156ecfd60ae4ed7acf45d7a4d9
thienthanh569 <thanhthien230409@gmail.com>
2024-12-24 10:27:22 +0700

    Add project files.

---
+++ bai 2 cong 2 so nguyen.cpp
@@ -0,0 +1,22 @@
+// bai 2 cong 2 so nguyen.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    
+    cout << "chuong trinh tinh tong 2 so nguyen\n";
+    int so1;
+        int so2;
+        cout << "Nhap so 1: ";cin >> so1; cout << "Nhap so 2: ";
+    
+   
+    cin >> so2;
+        int tong = so1 + so2;
+
+   cout << " tong 2 so tren la: " << tong;
+}
+
+/*ghi chu*/
